6cb2eaae570b02858abbe937810fa89b4b071e6b
Samael <samael803@gmail.com>
2020-04-30 17:08:20 +0300

    Import components and setup App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { sampleMarkdown } from './Modules/samples';
+
+import Preview from './Components/Preview';
+import Editor from './Components/Editor';
+import ControlPane from './Components/ControlPane';
+
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
 
-export default App;
+	const [text, setText] = useState(sampleMarkdown);
+
+	return (
+		<section className="main-stage">
+
+			<nav className="control-pane">
+				<ControlPane />
+			</nav>
+
+			<div className="editor-container">
+				<Editor text={text} setText={setText} />
+			</div>
+
+			<div className="preview-container">
+				<Preview text={text} />
+			</div>
+
+		</section>
+	);
+}
